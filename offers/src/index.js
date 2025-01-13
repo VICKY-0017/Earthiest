f914e36961c588f5f7580e7de67519f26c89ad80
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-01-13 23:45:55 +0530

    Update index.js

--- offers/src/index.js
+++ offers/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Ensure BrowserRouter is here
 import OfferProvider from './offerpg.jsx';
+import "./tailwind.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
