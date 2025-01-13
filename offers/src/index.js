2c2401329bb4d5dbcb77b052d8aae9e2697c963e
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-01-13 23:24:34 +0530

    Update index.js

--- offers/src/index.js
+++ offers/src/index.js
@@ -4,9 +4,9 @@ import OfferProvider from './offerpg.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <OfferProvider />
-  </React.StrictMode>
+  
 );
 
 
