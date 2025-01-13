00f026eb4de66433d702b511c51ed8c282e13674
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-01-13 23:19:06 +0530

    Update index.js

--- offers/src/index.js
+++ offers/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import OfferProvider from './offerpg';
+import OfferProvider from './offerpg.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
