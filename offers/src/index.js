11deb1d5c700104ea4aaad8e60c2f3e18efe8ad2
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-01-13 23:35:05 +0530

    Update index.js

--- offers/src/index.js
+++ offers/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Ensure BrowserRouter is here
 import OfferProvider from './offerpg.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <BrowserRouter>
     <OfferProvider />
-  
+  </BrowserRouter>
 );
-
-
